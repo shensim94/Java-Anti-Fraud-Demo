cd5c81946e5e70e1793f497cdc63dca0f8c66bc8
shensim94 <shensim@oregonstate.edu>
2023-12-11 12:35:04 -0800

    first comit

---
+++ src/main/java/com/example/AntiFraudDemo/AntiFraudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AntiFraudDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiFraudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiFraudDemoApplication.class, args);
+	}
+
+}
