713d940dc60dbd34e2e292c0fa543a8d92a79d73
shensim94 <shensim@oregonstate.edu>
2023-12-24 21:14:36 -0800

    refactored

--- src/main/java/com/example/AntiFraudDemo/user/UserController.java
+++ src/main/java/com/example/AntiFraudDemo/user/UserController.java
@@ -1,6 +1,5 @@
 package com.example.AntiFraudDemo.user;
 
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,39 +14,31 @@ import java.util.Optional;
 public class UserController {
 
     private final UserRepository users;
+
+    private final UserDetailsServiceImpl userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository users, PasswordEncoder passwordEncoder) {
+    public UserController(UserRepository users, UserDetailsServiceImpl userDetailsService, PasswordEncoder passwordEncoder) {
         this.users = users;
+        this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/list")
-    public ResponseEntity<Iterable<UserViewModel>> getUsers() {
+    public ResponseEntity<Iterable<UserDTO>> getUsers() {
         Iterable<User> allUsers = users.findAll();
-        ArrayList<UserViewModel> allUsersView = new ArrayList<>();
+        ArrayList<UserDTO> allUsersView = new ArrayList<>();
         for (User user: allUsers) {
-            allUsersView.add(new UserViewModel(user));
+            allUsersView.add(new UserDTO(user));
         }
         return new ResponseEntity<>(allUsersView, HttpStatus.OK);
     }
 
     @PostMapping("/user")
-    public ResponseEntity<UserViewModel> register(@RequestBody UserRegistrationRequest request) {
-        if (Strings.isBlank(request.name()) || Strings.isBlank(request.username()) || Strings.isBlank(request.password())) {
-            System.out.println("Bad request here");
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        if (users.findByUsernameIgnoreCase(request.username()).isPresent()) {
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }
-        System.out.println("Request is okay");
-        User user = new User();
-        user.setName(request.name());
-        user.setUsername(request.username());
-        user.setPassword(passwordEncoder.encode(request.password()));
-        users.save(user);
-        return new ResponseEntity<>(new UserViewModel(user), HttpStatus.CREATED);
+    public ResponseEntity<UserDTO> register(@RequestBody UserRegistrationRequest request) {
+
+        return userDetailsService.addUser(request);
+
     }
 
     @DeleteMapping("/user/{username}")
--- src/main/java/com/example/AntiFraudDemo/user/UserDetailsServiceImpl.java
+++ src/main/java/com/example/AntiFraudDemo/user/UserDetailsServiceImpl.java
@@ -1,5 +1,9 @@
 package com.example.AntiFraudDemo.user;
 
+import com.example.AntiFraudDemo.exception.ApiRequestException;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,6 +24,17 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                 .findByUsernameIgnoreCase(username)
                 .map(UserDetailsImpl::new)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+
+    public ResponseEntity<UserDTO> addUser(UserRegistrationRequest request) throws ApiRequestException {
+        if (Strings.isBlank(request.name()) || Strings.isBlank(request.username()) || Strings.isBlank(request.password())) {
+            throw new ApiRequestException("request is missing required fields");
+        }
+        if (userRepository.findByUsernameIgnoreCase(request.username()).isPresent()) {
+            throw new ApiRequestException("user is already present");
+        }
+        User user = new User(request.name(), request.username(), request.password());
+        return new ResponseEntity<>(new UserDTO(user), HttpStatus.CREATED);
     }
 
 }
