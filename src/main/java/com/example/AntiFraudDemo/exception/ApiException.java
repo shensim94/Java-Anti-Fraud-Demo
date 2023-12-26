4376b91085e9adab4cb2ea01f6e1657432c61561
shensim94 <shensim@oregonstate.edu>
2023-12-26 13:03:22 -0800

    converted from class to record

--- src/main/java/com/example/AntiFraudDemo/exception/ApiException.java
+++ src/main/java/com/example/AntiFraudDemo/exception/ApiException.java
@@ -1,31 +1,8 @@
 package com.example.AntiFraudDemo.exception;
 
-import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-public class ApiException {
-    private final String message;
-    private final HttpStatus httpStatus;
-    private final ZonedDateTime timeStamp;
-
-    public ApiException(String message, HttpStatus httpStatus, ZonedDateTime timeStamp) {
-        this.message = message;
-        this.httpStatus = httpStatus;
-        this.timeStamp = timeStamp;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
-
-    public ZonedDateTime getTimeStamp() {
-        return timeStamp;
-    }
+public record ApiException(String message, HttpStatus httpStatus, ZonedDateTime timeStamp) {
 }
